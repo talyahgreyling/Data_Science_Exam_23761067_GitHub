af23cc40b0ce44660df8bc9729728c0ac38414a5
talyahgreyling <talyahgreyling0@gmail.com>
2025-06-18 09:08:22 +0200

    Question3

--- Question3/code/identify_main_genre.R
+++
@@ -1,8 +0,0 @@
-# GOAL: Add column for main genre (assume first genre listed)  
-identify_main_genre = function(df,og_genre_col) {
-  df <- df %>% 
-    mutate(main_genre = sub(",.*", "", genres)) %>% 
-    mutate(main_genre = gsub("\\[|\\]|'", "", main_genre))
-  
-  df 
-}
