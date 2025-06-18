8d812bc7dd978323f7419ada7405919479347f22
talyahgreyling <talyahgreyling0@gmail.com>
2025-06-19 01:33:47 +0200

    Question4

--- Question4/code/counts_software.R
+++ Question4/code/counts_software.R
@@ -1,16 +1,27 @@
 #GOAL: count the amount of new billionaires through software companies per decade
 
 counts_software <- function(df, software_words){
-  
-  df_software_billions <- df %>%
-    mutate(sector_status = if_else(str_detect(
-      company.sector, regex(paste(software_words, collapse = "|"), ignore_case = TRUE)
-    ), true = "software",
-    false = "consumer"
-    )) %>%
-    filter(!is.na(sector_status))  %>% 
-    group_by(year, sector_status) %>%
-    summarise(count = n()) %>%
-    mutate(status = "US")
-  
+
+    df_software_billions <- df %>%
+        mutate(sector_status = if_else(
+            str_detect(company.sector, regex(
+                paste(software_words, collapse = "|"), ignore_case = TRUE
+            )),
+            true = "software",
+            false = "consumer"
+        )) %>%
+        filter(!is.na(sector_status))  %>%
+        mutate(new_status = if_else(
+            str_detect(
+                wealth.how.inherited,
+                regex("not inherited", ignore_case = TRUE)
+            ),
+            true = "new",
+            false = "inherited"
+        )) %>%
+        filter(new_status == "new") %>%
+        group_by(year, sector_status) %>%
+        summarise(count = n()) %>%
+        pivot_wider(names_from = sector_status, values_from = count) %>%
+        arrange(year)
 }
---
+++ Question4/code/draw_second_claim_software.R
@@ -0,0 +1,22 @@
+# GOAL: Creates a Lollipop plot of the amount of new billionaires through software vs consumer services (per decade)
+
+draw_second_claim_software <- function(df) {
+  ggplot(df) +
+    geom_segment(aes(
+      x = year,
+      xend = year,
+      y = consumer,
+      yend = software
+    ),
+    color = "grey") +
+    geom_point(aes(x = year, y = consumer),
+               color = rgb(0.2, 0.7, 0.1, 0.5),
+               size = 3) +
+    geom_point(aes(x = year, y = software),
+               color = rgb(0.7, 0.2, 0.1, 0.5),
+               size = 3) +
+    coord_flip() +
+    theme_ipsum() +
+    theme(legend.position = "right") +
+    labs(title = "Amount of New Billionaires Through Software vs Consumer Services (Per Decade)", x = "Years", y = "Billionaire Counts")
+}
