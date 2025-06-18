94751cd4ef8055146543c10a7d82410dd9f6fd1b
talyahgreyling <talyahgreyling0@gmail.com>
2025-06-18 23:22:24 +0200

    Question4

--- Question4/code/draw_first_claim.R
+++ Question4/code/draw_first_claim.R
@@ -1,6 +1,7 @@
+# GOAL: Creates a grouped barchart of the counts of new vs. inherited wealth per decade
 draw_first_claim <- function(df, input_title) {
-  ggplot(df, aes(fill = new_status, y = count, x = year)) + 
-    geom_bar(position="dodge", stat="identity") + 
+  ggplot(df, aes(fill = new_status, y = count, x = year)) +
+    geom_bar(position="dodge", stat="identity") +
     labs(title = input_title) +
     theme()
 }
