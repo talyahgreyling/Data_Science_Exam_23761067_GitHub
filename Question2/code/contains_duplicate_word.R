83cd538628c5d4042b49e48688f9083117045741
talyahgreyling <talyahgreyling0@gmail.com>
2025-06-18 14:59:49 +0200

    Question2

--- Question2/code/contains_duplicate_word.R
+++
@@ -1,4 +0,0 @@
-# GOAL: indicate if any duplicate words are present 
-contains_duplicate_word <- function(col_name) {
-  return(any(str_detect(col_name, regex(paste(duplicate_words, collapse = "|"), ignore_case = TRUE))))
-}
---
+++ Question2/code/no_duplicate_words.R
@@ -0,0 +1,4 @@
+# GOAL: indicate if any duplicate words are present 
+no_duplicate_words <- function(col_name) {
+  return(!any(str_detect(col_name, regex(paste(duplicate_words, collapse = "|"), ignore_case = TRUE))))
+}
